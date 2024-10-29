927760eed3cfbfb56b9504a1ffcddc32c964d70e
lens0021 <lorentz0021@gmail.com>
2024-10-29 10:31:24 +0900

    yazi: Remove the status bar tweak

--- yazi/sidebar/init.lua
+++
@@ -1,9 +0,0 @@
---- Makes the status bar way more readable (but you don't need lua as a dependency, so no worries)
-function Status:render(area)
-	self.area = area
-
-	local line = ui.Line { self:percentage(), self:position() }
-	return {
-		ui.Paragraph(area, { line }):align(ui.Paragraph.CENTER),
-	}
-end
